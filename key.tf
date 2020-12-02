ad6f4f5e60c077dc762debecfec2d8f147de8b49
felipe <aronakamoto@outlook.com>
2020-12-02 01:18:10 -0300

    Feat: Key

---
+++ key.tf
@@ -0,0 +1,8 @@
+##################################################################
+# Launch configuration and autoscaling group.
+##################################################################
+resource "aws_key_pair" "aron-key" {
+  provider = aws.us-east-1
+  key_name   = "aron-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDUav5e59nrtmeVuHET9VBHUngY7yW0Dk6deoWRHeIsL4/1Jrs9TCAP2Ix+FqQz1mFeCzSRFydooUNQ0NqoUfLY5atDB104IYm0RlppPZSOJFJVIPYefNKDVsYYdVXfuCaSW52AyWUpuYt0cO4sRQqZGPjurOy2xPW4TP/cybiu5+YC13l9A3DsYEsWfYif4+lkrEOnt47MBbIAx1oSHEwKP9EhZofNg8Qb/DO6vISILCJ07QyNIHxcMbwth4ejslrjMCZjCMJe/sBuGO5fYDhKV9FUSgPHCLRFo8l5WMaeDyrdb8EkujckFf30dr1zjA0ookEAqVplr+iW6RRI2Ftr felipe@felipe-340XAA-350XAA-550XAA"
+}
